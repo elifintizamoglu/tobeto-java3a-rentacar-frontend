842b827a1b739d154fa5b24afbe36bc0157b31c5
elifintizamoglu <elifintizam@gmail.com>
2024-03-31 17:10:23 +0300

    components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  // state
   title = 'JavaFS3ARentACar';
+  counter: number = 0;
+
+  incrementCounter() {
+    ++this.counter;
+  }
 }
